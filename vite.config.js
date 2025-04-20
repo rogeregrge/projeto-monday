14bf46f96d2597feba4fa71fa3b29da39b505b54
rogeregrge <rogervidal94@gmail.com>
2025-04-20 18:27:26 -0300

    vite config update, boarditemslist update with vibe core changes

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: {}, //
+  },
   server: {
-    host: true, // permite acesso externo
+    host: true,
     allowedHosts: [
       '058c-2804-1b3-a943-e595-80d6-7e33-a288-2eda.ngrok-free.app'
     ]
